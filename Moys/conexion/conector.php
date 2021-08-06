d86fc89f402aebfdb0ad58458ef65f3b6a94e985
Loles678 <oscar.ramirezh678@gmail.com>
2021-08-05 22:19:36 -0500

    Modificación nueva de conector

--- Moys/conexion/conector.php
+++ Moys/conexion/conector.php
@@ -1,5 +1,5 @@
 <?php
-
+//modificación de prueba
 $mysqli = new mysqli("localhost", "root", "Loles3puntos.", "moys");
 if ($mysqli->connect_errno) {
     echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
