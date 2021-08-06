1ea33933c9890b328178cc6b79988bb636568675
Loles678 <oscar.ramirezh678@gmail.com>
2021-08-05 22:49:06 -0500

    conector

--- Moys/conexion/conector.php
+++ Moys/conexion/conector.php
@@ -1,8 +1,17 @@
+
 <?php
-//modificación de prueba
-$mysqli = new mysqli("localhost", "root", "Loles3puntos.", "Moys");
-if ($mysqli->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'Loles3puntos.');
+define('DB_NAME', 'Moys');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-echo $mysqli->host_info . "\n";
 ?>
