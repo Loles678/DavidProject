f83c18d7ca73de756f66c210ca60143ea7cf0e80
Loles678 <oscar.ramirezh678@gmail.com>
2021-08-13 02:46:41 -0500

    s

--- Moys/principal.php
+++ Moys/principal.php
@@ -6,15 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
    <!-- Bootstrap CSS -->
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+   <!-- Place your kit's code here -->
+   <script src="https://kit.fontawesome.com/4ddc389f8f.js" crossorigin="anonymous"></script>
+   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap"
+rel="stylesheet"/>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
    <title>Los Moys - Pagina de inicio</title>
  </head>
-    <!-- Place your kit's code here -->
-    <script src="https://kit.fontawesome.com/4ddc389f8f.js" crossorigin="anonymous"></script>
-<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital@1&display=swap"
-rel="stylesheet"/>
-    <title>Aplicacion Web</title>
-  </head>
   <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container px-4 px-lg-5">
@@ -37,7 +36,7 @@ rel="stylesheet"/>
                         <button class="btn btn-outline-dark" type="submit">
                             <i class="bi-cart-fill me-1"></i>
                             Carrito
-                            <span class="badge bg-dark text-white ms-1 rounded-pill">0</span>
+                            <span class="badge bg-dark text-white ms-1 rounded-pill"><a id="count">0</a></span>
                         </button>
                     </form>
                 </div>
@@ -51,80 +50,138 @@ rel="stylesheet"/>
                 </div>
             </div>
         </header>
-    <div class="container-fluid mt-5">
-      <div class="row">
-        <!--layout izquierdo-->
-    </div>
-    <br />
-    <section class="py-5">
-            <div class="container px-4 px-lg-5 mt-5">
-                <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-                  <?php
-                  $resultado = mysqli_query($link, "SELECT * FROM platillo");
+        <div class="container-fluid mt-5">
+          <div class="row">
+            <!--layout izquierdo-->
+          </div>
+          <br />
+          <section class="py-5">
+                <div class="container px-4 px-lg-5 mt-5">
+                    <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                      <?php
+                      $resultado = mysqli_query($link, "SELECT * FROM platillo");
 
-                  if( $resultado ){
+                      if( $resultado ){
 
-                    //Ahora valida que la consuta haya traido registros
-                    if( mysqli_num_rows( $resultado ) > 0){
+                        //Ahora valida que la consuta haya traido registros
+                        if( mysqli_num_rows( $resultado ) > 0){
 
-                      //Mientras mysqli_fetch_array traiga algo, lo agregamos a una variable temporal
-                      while($fila = mysqli_fetch_array( $resultado ) ){
+                          //Mientras mysqli_fetch_array traiga algo, lo agregamos a una variable temporal
+                          while($fila = mysqli_fetch_array( $resultado ) ){
 
-                        //Ahora $fila tiene la primera fila de la consulta, pongamos que tienes
-                        //un campo en tu DB llamado NOMBRE, así accederías
-                        echo '<div class="col mb-5">
-                            <div class="card h-100">
-                                <!-- Sale badge-->
-                                <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Especial</div>
-                                <!-- Product image-->
-                                <img class="card-img-top" src="'.$fila["img_plat"].'" alt="..." />
-                                <!-- Product details-->
-                                <div class="card-body p-4">
-                                    <div class="text-center">
-                                        <!-- Product name-->
-                                        <h5 class="fw-bolder">'.$fila['tipo_plat'].'</h5>
-                                        <!-- Product price-->
-                                        '.$fila['prec_platillo'].'
+                            //Ahora $fila tiene la primera fila de la consulta, pongamos que tienes
+                            //un campo en tu DB llamado NOMBRE, así accederías
+                            echo '<div class="col mb-5">
+                                <div class="card h-100">
+                                    <!-- Sale badge-->
+                                    <div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Especial</div>
+                                    <!-- Product image-->
+                                    <img class="card-img-top" src="'.$fila["img_plat"].'" alt="..." />
+                                    <!-- Product details-->
+                                    <div class="card-body p-4">
+                                        <div class="text-center">
+                                            <!-- Product name-->
+                                            <h5 class="fw-bolder">'.$fila['tipo_plat'].'</h5>
+                                            <!-- Product price-->
+                                            '.$fila['prec_platillo'].'
+                                        </div>
+                                    </div>
+                                    <!-- Product actions-->
+                                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                                        <div class="text-center"><a name="carrito" id="'.$fila['cve_plat'].'" class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
                                     </div>
                                 </div>
-                                <!-- Product actions-->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Agregar al carrito</a></div>
-                                </div>
-                            </div>
-                        </div>';
+                            </div>';
+                          }
+
+                        }
                       }
+                      //cve_plat,tipo_plat,prec_platillo,cost_platillo,img_plat
 
-                    }
-                  }
-                  //cve_plat,tipo_plat,prec_platillo,cost_platillo,img_plat
+                      mysqli_close($link);
 
-                  mysqli_close($link);
+                       ?>
 
-                   ?>
-
+                    </div>
                 </div>
+            </section>
+        <br />
+        <!--Footer-->
+        <footer id="main-footer" class="bg-dark text-white mt-5 p-5">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                <p class="lead text-center">LosMoys S.A. de C.V. Todos Los Derechos Reservados &copy; <span>App Web</span></p>
+              </div>
             </div>
-        </section>
-    <br />
-    <!--Footer-->
-    <footer id="main-footer" class="bg-dark text-white mt-5 p-5">
-      <div class="container">
-        <div class="row">
-          <div class="col">
-            <p class="lead text-center">LosMoys S.A. de C.V. Todos Los Derechos Reservados &copy; <span>App Web</span></p>
           </div>
-        </div>
+        </footer>
       </div>
-    </footer>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
-    -->
+    <script type="text/javascript">
+    //Creamos funcion a partir del click de las etiqueas <a> con el cual podremos hacer uso del carrito
+    var array_carrito = new Array();
+     $('a').click(function(){
+       if($(this).attr('name') == "carrito"){
+         var cve_platillo = $(this).attr('id');
+         var contador = Number.parseInt($('#count').html()) + 1;
+         $('#count').html(contador);
+
+         if(array_carrito == null ||array_carrito == ""){
+           array_carrito[1] ={"cve_plat":cve_platillo,"cont":1}
+         }else{
+           for (var i = 1; i <= array_carrito.length; i++) {
+             var plat = array_carrito[i].cve_plat;
+             if(plat == cve_platillo){
+               var suma = Number.parseInt(array_carrito[i].cont+1);
+
+               array_carrito.map(function(dato){
+                 if(dato.cve_plat == cve_platillo){
+                   dato.cont = suma;
+                 }
+                 return dato;
+               });
+             }
+             else {
+               array_carrito[i+1] ={"cve_plat":cve_platillo,"cont":1}
+             }
+           }
+         }
+         console.log(array_carrito);
+         /*$.ajax({
+           // la URL para la petición
+           url : 'conexion/setOrden.php',
+           // la información a enviar
+           // (también es posible utilizar una cadena de datos)
+           data : { 'cve_plat' : cve_platillo, },
+           // especifica si será una petición POST o GET
+           type : 'GET',
+           // el tipo de información que se espera de respuesta
+           dataType : 'json',
+           // código a ejecutar si la petición es satisfactoria;
+           // la respuesta es pasada como argumento a la función
+           success : function(json) {
+            $('<h1/>').text(json.title).appendTo('body');
+            $('<div class="content"/>')
+                .html(json.html).appendTo('body');
+          },
+          // código a ejecutar si la petición falla;
+          // son pasados como argumentos a la función
+          // el objeto de la petición en crudo y código de estatus de la petición
+          error : function(xhr, status) {
+            alert('Disculpe, existió un problema');
+          },
+          // código a ejecutar sin importar si la petición falló o no
+          complete : function(xhr, status) {
+            alert('Petición realizada');
+          }
+        });*/
+         //alert("funciona!"+elem);
+       }
+     });
+    </script>
   </body>
   </html>
